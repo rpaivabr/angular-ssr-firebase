fe3718a477dcd404e4047494694683885b1a2092
Rafael Paiva <r.paivabr@gmail.com>
2019-12-06 11:12:35 -0300

    add universal for ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'heed-site';
+  data = {
+    name: 'Michael Jordan',
+    bio: 'Former baseball player',
+    image: 'https://goo.gl/hfvwfq',
+    url: 'https://www.heedsolutions.com.br'
+  };
+
+  constructor(private meta: Meta) {}
+
+  ngOnInit() {
+    this.meta.addTags([
+      { property: 'og:title', content: this.data.name },
+      { property: 'og:description', content: this.data.bio },
+      { property: 'og:image', content: this.data.image },
+      { property: 'og:url', content: this.data.image },
+      { name: 'twitter:title', content: this.data.name },
+      { name: 'twitter:description', content: this.data.bio },
+      { name: 'twitter:image', content: this.data.image },
+      { name: 'twitter:card', content: 'summary_large_image' },
+    ]);
+  }
 }
